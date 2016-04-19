98da675b86226c409c7c9b6978100f02d3f535b0
Serhiy Kudryk <serhii.kudryk@perfectial.com>
2016-04-19 19:16:18 +0300

    expandTree,editBook handlers; support for passing data between parent-child; basic styles

--- app/assets/javascripts/books.js.jsx
+++ app/assets/javascripts/books.js.jsx
@@ -1,15 +1,41 @@
 var Book = React.createClass({
+   getInitialState: function() {
+    return {collapsed: true, indicator: '+'};
+  },
   render: function() {
+    var self = this;
     return (
-      <div className="book">
+      <div className="book-container" id={'book_' + this.props.id}>
         <div className="book-heading">
-          <h3 className="book-title">{this.props.title}</h3>
+          <h3 className="book-title">
+            <a className="book-collapse" onClick={self.expandTree}>{self.state.indicator}</a>
+            <a title={this.props.category} onClick={self.editBook}> {this.props.title}</a>
+          </h3>
         </div>
         <div className="book-pages">
-          category: <span className="book-title">{this.props.category}</span>
+          { this.props.pages.map(function(page, i) {
+            return (
+              <div key={i}><a onClick={this.expandTree}>{page.title}</a></div>
+            );
+          })}
+        </div>
+        <div className="book-edit">
+           <BookForm onBookSubmit={self.props.parent.handleBookSubmit} caps="Edit book" book={this.props} />
         </div>
       </div>
     );
+  },
+  expandTree: function(event) {
+    var book_id = this.props.id;
+    console.log('this',this);
+    $('#book_' + book_id + ' .book-pages').toggle();
+    this.setState({indicator: (this.state.indicator == '+') ? '-' : '+'});
+    return;
+  },
+  editBook: function(event) {
+    var book_id = this.props.id;
+    $('#book_' + book_id + ' .book-edit').toggle();
+    return;
   }
 });
 
@@ -59,7 +85,7 @@ var BooksBox = React.createClass({
     return (
       <div className="bookBox">
         <h2>Books</h2>
-        <BookList data={this.state.data} />
+        <BookList data={this.state.data} parent={this} />
         <hr />
         <BookForm onBookSubmit={this.handleBookSubmit} />
       </div>
@@ -70,11 +96,11 @@ var BooksBox = React.createClass({
 var BookList = React.createClass({
   render: function() {
     console.log('props:',this.props);
+    var self = this;
     var books = this.props.data.map(function(book, index) {
       return (
         // `key` is a React-specific concept to recognize dynamic children items 
-        <Book date={book.updated_at} category={book.category} title={book.title} name={book.name} key={index}>
-          {book.pages_list}
+        <Book date={book.updated_at} category={book.category} id={book.id} pages={book.pages} title={book.title} title={book.title} parent={self.props.parent} key={index}>
         </Book>
       );
     });
@@ -100,18 +126,19 @@ var BookForm = React.createClass({
     return;
   },
   render: function() {
+    var book = this.props.book || {};
     return (
       <div className="panel">
-        <div className="book-heading">Add a Book</div>
+        <div className="book-heading"> {this.props.caps || 'Add a Book' }</div>
         <div className="book-body">
           <form className="bookForm " onSubmit={this.handleSubmit}>
             <div className="form-group">
               <label className="control-label" forName="titleInput">Name</label>
-              <input type="text" id="titleInput" className="form-control" placeholder="name" ref="title" />
+              <input type="text" id="titleInput" className="form-control" placeholder="name" ref="title" defaultValue={book.title} />
             </div>
             <div className="form-group">
               <label className="control-label" forName="categoryInput">Category</label>
-              <input type="text" className="form-control" id="categoryInput" rows="3" placeholder="Category" ref="category" />
+              <input type="text" className="form-control" id="categoryInput" rows="3" placeholder="Category" ref="category" defaultValue={book.category} />
             </div>
             <input type="submit" className="btn btn-primary" value="Create" />
           </form>
